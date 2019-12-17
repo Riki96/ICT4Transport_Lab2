12824c03836d1512d52ef77054b44009685dcf2c
Riccardo <sappariccardo@gmail.com>
2019-12-17 18:33:57 +0100

    Dataset created

--- ARIMA_Class.py
+++ ARIMA_Class.py
@@ -1,14 +1,14 @@
 import pymongo as pm
 import datetime
 from scipy import stats
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sb
 
 class PreProcessing :
-	def __init__():
-		
+	def __init__(self):
 		client = pm.MongoClient('bigdatadb.polito.it',
 			ssl=True,
 			authSource = 'carsharing',
@@ -16,33 +16,28 @@ class PreProcessing :
 			)
 		self.db = client['carsharing']
 		self.db.authenticate('ictts', 'Ictts16!')
-
 		self.per_bk = self.db['PermanentBookings']
-		
-		# self.act_bk = self.db['ActiveBookings']
+		self.act_bk = self.db['ActiveBookings']
 
-
-		
-
-	def mongoDB(self,city, start, end):
+	def mongoDB(self, city, unix_start, unix_end):
 		pipeline = [{
-						'$match':{
-							'city':c,
-							'init_time':{'$gte':unix_start,'$lte':unix_end}
-							}
+					'$match':{
+						'city':city,
+						'init_time':{'$gte':unix_start,'$lte':unix_end}
+						}
 					},
 						#FILTERING PORTION OF PIPELINE-----------------
 					{
 						'$project':{
-							'init_date':1,
-							'init_time':1,
-							'final_time':1,
-							'plate':1,
-							'city':1,
-							'durata': { '$divide': [ { '$subtract': ["$final_time", "$init_time"] }, 60 ] },
-							'dist_lat':{'$abs':{'$subtract': [{'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 0]}, 0]}, {'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 1]}, 0]}]}},
-							'dist_long':{'$abs':{'$subtract': [{'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 0]}, 1]}, {'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 1]}, 1]}]}},
-							}
+						'init_date':1,
+						'init_time':1,
+						'final_time':1,
+						'plate':1,
+						'city':1,
+						'durata': { '$divide': [ { '$subtract': ["$final_time", "$init_time"] }, 60 ] },
+						'dist_lat':{'$abs':{'$subtract': [{'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 0]}, 0]}, {'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 1]}, 0]}]}},
+						'dist_long':{'$abs':{'$subtract': [{'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 0]}, 1]}, {'$arrayElemAt':[{'$arrayElemAt': [ "$origin_destination.coordinates", 1]}, 1]}]}},
+						}
 					},
 					{
 						'$match':{
@@ -51,41 +46,51 @@ class PreProcessing :
 								{'dist_lat':{'$gte':0.0003}},
 								],
 							'durata':{'$lte':180,'$gte':2},
-							}
+						}
 					},
 						#END OF FILTERING
 					{ 
 						"$group": {
-    							"_id":{
-    									'hour':{'$hour':'$init_date'},
-    									'day':{'$dayOfYear':'$init_date'},
-    									'plate':'$plate'
-    									}
-    							
-    						}
+							"_id":{
+								'hour':{'$hour':'$init_date'},
+								'day':{'$dayOfYear':'$init_date'},
+								# 'plate':'$plate'
+								},
+							's':{'$sum':1}	
+    					}
 		        	}]
 
 		result = self.per_bk.aggregate(pipeline)
+		output = list(result)
+		return output
 
-	def dataset_creation(cities, start, end):
+	def dataset_creation(self, cities, start, end, startNY, endNY):
 		for c in cities:
+			print(c)
 			if c == 'New York City':
-				start = datetime.datetime(start, timedelta=-8)
-				end = datetime.datetime(end, timedelta=-8)
-			else:
-				start = datetime.datetime(start)
-				end = datetime.datetime(end)
-			data = self.mongoDB(c, start, end)
-			df = pd.DataFrame(data)
-			c = c.trim()
-			df.to_excel('Data_{}'.format(c))
+				start = startNY
+				end = endNY
 
-class DataMining ():
-	def __init__(self,dataset):
-		self.data = dataset
+			unix_start = time.mktime(start.timetuple())
+			unix_end = time.mktime(end.timetuple())
 
-	def model_specifications():
+			data = self.mongoDB(c, unix_start, unix_end)
+			df = pd.DataFrame({
+				'Day':[i['_id']['day'] for i in data],
+				'Hour':[i['_id']['hour'] for i in data],
+				'Total':[i['s'] for i in data]
+				})
+			df = df.sort_values(['Day', 'Hour'])
+			print(df)
+			c = c.strip()
+			df.to_excel('Data_{}.xlsx'.format(c))
 
-	def performances():
+# class DataMining ():
+# 	def __init__(self,dataset):
+# 		self.data = dataset
 
-	def prediction_results():
+# 	def model_specifications():
+
+# 	def performances():
+
+# 	def prediction_results():
