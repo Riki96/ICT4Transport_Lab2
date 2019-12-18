8924e1b0a8a3ca434f1c0cd812b88e00fa39db38
piotre13 <Orteip13>
2019-12-18 17:17:58 +0100

    yo finished

--- ARIMA_Class.py
+++ ARIMA_Class.py
@@ -5,10 +5,15 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import random
 import statsmodels as sm
 import seaborn as sb
 import os
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from statsmodels.tsa.arima_model import ARIMA
 
 class PreProcessing :
 	def __init__(self):
@@ -68,8 +73,8 @@ class PreProcessing :
 		# print(output)
 		return output
 
-	def dataset_creation(self, cities, start, end, startNY, endNY):
-		for c in cities:
+	def dataset_creation(self, self.cities, start, end, startNY, endNY):
+		for c in self.cities:
 			print(c)
 			if c == 'New York City':
 				start = startNY
@@ -91,11 +96,10 @@ class PreProcessing :
 			df.to_excel('data/Data_{}.xlsx'.format(c))
 
 class DataMining ():
-	def __init__(self, d=0, p=0, q=0):
-		self.d = d
-		self.p = p
-		self.q = q
-
+	def __init__(self, cities):
+		sb.set_style('darkgrid')
+		self.cities = cities
+		self.time_series ={}
 	def data_filling(self, dataset, c):
 		hours = []
 		for i in range(31):
@@ -123,8 +127,8 @@ class DataMining ():
 		print(final_dataset)
 		final_dataset.to_excel('data/Data_{}.xlsx'.format(c))
 		return final_dataset
-
-	def system_plotting(self, cities, date):
+	
+	def system_plotting(self,date):
 		x_lab=[]
 		for i in range(31):
 			date = date + datetime.timedelta(days=i)
@@ -140,14 +144,13 @@ class DataMining ():
 			print(len(i))
 			plt.figure(figsize=(15,5))
 			plt.plot(list(i.loc[:,'Total']))
-			plt.title(cities[cnt]+' Time Series')
+			plt.title(self.cities[cnt]+' time series')
 			plt.grid()
 			plt.xticks(x_axis,x_lab, rotation=45)
 			plt.show()
 			cnt+=1
 
 	def plot_correlations(self, plot=True, save=True):
-		from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 		df = pd.read_excel('data/Data_Torino.xlsx')
 		data = df.loc[:, 'Total']
 
@@ -176,11 +179,118 @@ class DataMining ():
 		return Torino, Amsterdam, NY
 	
 
-	def model_specifications():
-		pass
+	def arima_training(self):
+		df_TO, df_AM, df_NY = self.files_opening()
+		dataframe = [df_TO, df_AM, df_NY]
+		
+		lag_order = (1,2,3,4)
+		MA_order = (1,2,3,4)
+		#lag_order = (1,2)
+		#MA_order = (1,2)
+		results = {}
 
-	def performances():
-		pass
 
-	def prediction_results():
-		pass
+		
+		cnt = 0
+		for df in dataframe:
+			df = df['Total']
+			df += np.random.random_sample(len(df))/10
+			X = df.values.astype(float)
+			self.time_series[self.cities[cnt]]= X
+			train_size = 24*7*3
+			test_len = len(df.index) - train_size
+			predictions = np.zeros((len(lag_order),len(MA_order),test_len)) # to store outputs
+			#df.head()
+			#print (X)
+			print('****************')
+			print ('Study case:', self.cities[cnt])
+			print('****************')
+
+
+			train, test = X[0:train_size], X[train_size:(train_size+test_len)]
+			history = [x for x in train]
+
+			for p in lag_order:
+				for q in MA_order:
+					print('Testing ARIMA order (%i,%i,%i)' % (p,0,q))
+
+					for t in range(0, test_len):
+					
+						model = ARIMA(history,order=(p,0,q))
+						model_fit = model.fit(disp=0, maxiter=500, method='css')
+						output = model_fit.forecast()
+						yhat = output[0] 
+						predictions[lag_order.index(p)][MA_order.index(q)][t]=yhat
+						obs = test[t] # slide over time, by putting now+1 into past
+						#print('--> t=%i: prediction=%.2f, actual=%i, error=%1.2f (%.2f%%) -- ' % (t, yhat, obs, yhat-obs, 100*(yhat-obs)/obs))
+						
+						history.append(obs)
+						history = history[1:] # linea sliding/ expanding, if expanding comment
+			
+			results[self.cities[cnt]] = self.performances(predictions,test,self.cities[cnt], lag_order, MA_order)
+			
+			cnt+=1
+
+		print (results)
+		return results
+			
+
+	def performances(self,predictions,test,city, lag_order,MA_order):
+		order_list =[]
+		MAE_list =[]
+		MAPE_list =[]
+		MSE_list =[]
+		R2_list =[]
+
+		for p in lag_order: # now for each model compute performance and plot predictions
+			plt.figure(figsize=(15,5))
+			plt.plot(test, color='black', label='Orig', linewidth = 2)# plot the real time series
+			
+			for q in MA_order:
+				order_list.append('('+str(p)+',0,'+str(q)+')')
+				MAE_list.append(mean_absolute_error(test, predictions[lag_order.index(p)][MA_order.index(q)]))
+				MAPE_list.append(mean_absolute_error(test, predictions[lag_order.index(p)][MA_order.index(q)]) / test.mean()*100)
+				MSE_list.append(mean_squared_error(test, predictions[lag_order.index(p)][MA_order.index(q)]))
+				R2_list.append(r2_score(test, predictions[lag_order.index(p)][MA_order.index(q)]))
+
+				plt.plot(predictions[lag_order.index(p)][MA_order.index(q)], label='p=%i,q=%i' % (p,q) ) 
+			
+			plt.legend()
+			plt.title(str(city)+': PREDICTION with p='+str(p))
+			plt.savefig('plots/model_fitting_'+str(city)+'_P='+str(p)+'.png')
+
+		performances = pd.DataFrame({	
+										'order': order_list,
+										'MAE': MAE_list,
+										'MAPE': MAPE_list ,
+										'MSE': MSE_list,
+										'R2': R2_list
+									})
+		#print (performances)
+
+		return performances.loc[performances['MSE'].idxmin()]['order']
+
+		
+		#return city, P, Q
+
+
+	def results_predicting(self):
+		orders = self.arima_training()
+
+		for c in self.cities:
+			model = ARIMA(self.time_series[c], order=orders[c])
+			fig, ax = plt.subplots()
+			ax = df.loc[650:].plot(ax=ax) # plot the data from step 650 up to now
+			#	plot now the predicted data for the future days – we have 720 sample – so let’s go up to 780 
+			fig = model_fit.plot_predict(650, 780, dynamic=False, ax=ax, plot_insample=False)
+
+
+
+
+
+
+
+
+
+
+
